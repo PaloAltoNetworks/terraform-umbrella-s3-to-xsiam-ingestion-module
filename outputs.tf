062612676d915cbda5e168a7925eb119deed1c03
James Holland <6574404+jamesholland-uk@users.noreply.github.com>
2025-05-12 20:22:19 +0800

    test github actions

--- outputs.tf
+++ outputs.tf
@@ -39,5 +39,9 @@ output "user2_access_key" {
 output "user2_secret_key" {
   value       = aws_iam_access_key.user2_access_key.secret
   sensitive   = true
-  description = "Secret key for second user, if required"
+<<<<<<< HEAD
+      ==
+  des   description = "Secret key for second user, if required"
+=====cription = "Secret key for second user, if required"
+>>>>>>> 55a1cab3754e227d9d0f4f05608ee152195dede8
 }
